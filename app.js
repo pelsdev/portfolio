204e51f4bfda7e4dd85006760d4f568a0ea42723
pelsdev <pelumiajiteru@gmail.com>
2023-12-19 13:24:11 +0100

    update on styling and functionality with CSS and JavaScript

---
+++ app.js
@@ -0,0 +1,6 @@
+function handleCreatePost() {
+    document.getElementById("post-form-container").style.display = "block";
+  }
+  function hidePostForm() {
+    document.getElementById("post-form-container").style.display = "none";
+  }
